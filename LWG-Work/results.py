b6d2fd8d49c8e8ab1babf2da92694b5551051fa2
lwgraham5678 <127980210+lwgraham5678@users.noreply.github.com>
2023-04-23 20:44:18 -0600

    -m work 4/23

--- LWG-Work/results.py
+++ LWG-Work/results.py
@@ -2,19 +2,20 @@ import Testdefs as td
 from TestConfig import P
 import scipy as sp
 import numpy as np
+import math as m
 
 
-originaldataframe = P.totalmenXnum_bmsm
-row_limit = 300
-column_limit = 300
+originaldataframe = P.numpars1Xnum_bmsm
+row_limit = 100
+column_limit = 100
 num_generated_sequences = 10
 sequence_length = 1000
-Configuration_itterations = 100
-num_networks_per_ds = 10
-num_alpha_parameters = 10
+Configuration_itterations = 10
+num_networks_per_ds = 50
+num_alpha_parameters = 50
 data_list = []
 dist = sp.stats.poisson
-bounds = {'mu' : (0, 200)}
+bounds = {'mu' : (0, 1000)}
 
 dataframe = td.RemoveHighValuedNodes(originaldataframe, row_limit, column_limit)
 
@@ -28,32 +29,36 @@ column_fit = sp.stats.fit(dist, sequences[1], bounds)
 row_mu, row_loc = row_fit.params
 column_mu, column_loc = column_fit.params
 
-row_std = dist.std(row_mu)
-column_std = dist.std(column_mu)
-row_mean = dist.mean(row_mu)
-column_mean = dist.mean(column_mu)
+row_std = dist.std(row_mu, row_loc)
+column_std = dist.std(column_mu, column_loc)
+row_mean = row_mu
+column_mean = column_mu
 
 
-std_matrix = np.array([[row_mean, 0],[0, column_mean]])
+std_matrix = np.array([[m.log(row_std), 0],[0, m.log(column_std)]])
 
 for alpha in alpha_parameter_list:
 
-    print(alpha)
+    #print(alpha)
     
     Omega = np.array([[1, alpha], [alpha, 1]])
 
+    #Sigma = np.array([[(row_std**2)*alpha, row_std*column_std], [row_std*column_std, (column_std**2)*alpha]]) 
     Sigma = np.matmul(std_matrix, np.matmul(Omega, std_matrix))
     
-    log_of_means_dist = sp.stats.multivariate_normal([row_mean, column_mean], Sigma)
+    log_of_means_array = sp.stats.multivariate_normal.rvs(mean = [m.log(row_mean), m.log(column_mean)], cov = Sigma)
 
-    log_of_means_array = log_of_means_dist.rvs()
+    #log_of_means_array = log_of_means_dist.rvs()
     
     means_array = np.exp(log_of_means_array)
+    #print(means_array)
 
-    row_sequence = list(dist.rvs(mu = means_array[0], loc = row_loc, size = sequence_length))
-    column_sequence = list(dist.rvs(mu = means_array[1], loc = column_loc, size = sequence_length))
+    row_sequence = list(dist.rvs(mu = means_array[0], size = sequence_length))
+    column_sequence = list(dist.rvs(mu = means_array[1], size = sequence_length))
     correlation_coeff, p_value = sp.stats.spearmanr(row_sequence, column_sequence)
-    print(row_sequence, column_sequence)
+    #print(correlation_coeff)
+    #print(max(row_sequence), min(row_sequence))
+    #print(max(column_sequence), min(column_sequence))
     
     overlaps = []
     num_edges_list = []
